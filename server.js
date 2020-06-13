d538610811b4a0d97505066e0c422590846130d5
Ibrahim <ibrahim.tigrek@gmail.com>
2020-06-13 17:54:27 +0300

    added server port log

--- server.js
+++ server.js
@@ -3,4 +3,5 @@ const port = process.env.PORT || 3000;
 const app = require('./app');
 const server = http.createServer(app);
 
+console.log(`server is listening at port: ${port}`);
 server.listen(port);
